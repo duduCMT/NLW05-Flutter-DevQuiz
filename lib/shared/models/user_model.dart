1d8f1d98d3d39ea3efd609ea71f57d7146ec94b1
Eduardo Lima Costa <dev.eduardo.lc@gmail.com>
2021-04-21 22:28:53 -0300

    Estruturação do model para usuário

---
+++ lib/shared/models/user_model.dart
@@ -0,0 +1,11 @@
+class UserModel {
+  String name;
+  String photoUrl;
+  final int score;
+  
+  UserModel({
+    required this.name,
+    required this.photoUrl,
+    required this.score,
+  });
+}
